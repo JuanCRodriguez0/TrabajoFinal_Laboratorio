d2a42fdaacdc102eddfd08f4421d2b12a1332831
JuanCRodriguez0 <juancruzrodriguez0@gmail.com>
2025-11-18 22:20:17 -0300

    BD última
    Co-Authored-By: Agustin Mazza <agus-mazza@hotmail.com>
    Co-Authored-By: HYJeremias <jeremiashoyo035@gmail.com>
    Co-Authored-By: Mauricio Damián Reta <mauricio.reta.03@gmail.com>
    Co-Authored-By: Facü <206273675+frikiFazzyy31@users.noreply.github.com>
    Co-Authored-By: jeremiassflores <227914104+jeremiassflores@users.noreply.github.com>

--- cinemacentro_final.sql
+++ cinemacentro_final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-11-2025 a las 05:42:21
+-- Tiempo de generación: 19-11-2025 a las 02:19:51
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -44,7 +44,17 @@ CREATE TABLE `asiento` (
 INSERT INTO `asiento` (`codAsiento`, `fila`, `numero`, `estado`, `codProyeccion`) VALUES
 (68, 'A', 3, 0, 18),
 (69, 'A', 4, 0, 18),
-(70, 'A', 3, 0, 19);
+(70, 'A', 3, 0, 19),
+(71, 'B', 3, 1, 18),
+(72, 'A', 3, 1, 22),
+(73, 'A', 4, 1, 19),
+(74, 'A', 5, 1, 19),
+(75, 'A', 1, 1, 19),
+(76, 'A', 2, 1, 19),
+(77, 'B', 3, 1, 19),
+(78, 'B', 4, 1, 19),
+(79, 'C', 3, 1, 19),
+(80, 'C', 4, 1, 19);
 
 -- --------------------------------------------------------
 
@@ -66,12 +76,12 @@ CREATE TABLE `comprador` (
 --
 
 INSERT INTO `comprador` (`dni`, `nombre`, `fechaNac`, `password`, `medioDePago`, `estado`) VALUES
-(22222222, 'Facundo Lopez Cozzella', '2000-11-17', 'facundo', 'Efectivo', 0),
-(38751080, 'Jeremias Sirur Flores', '1995-03-13', '38751080', 'Tarjeta Credito', 0),
-(42238525, 'Agustin Mazza', '2000-04-20', '42238525', 'Tarjeta Credito', 1),
-(42357107, 'Juan Rodriguez', '2000-01-23', 'juan', 'Efectivo', 0),
-(44444222, 'Jeremias Hoyo', '2004-08-05', 'jerechikito', 'Tarjeta Credito', 0),
-(44752772, 'Mauricio Reta', '2003-04-13', '44752772', 'Efectivo', 1);
+(38751080, 'Jeremias Sirur Flores', '1995-03-13', '38751080', 'Tarjeta Credito', 1),
+(42238525, 'Agustin Mazza', '1999-12-11', '42238525', 'Tarjeta Credito', 1),
+(42357107, 'Juan Rodriguez', '2000-01-23', 'juan', 'Efectivo', 1),
+(42778631, 'Facundo Lopez Cozzella', '2000-08-13', 'facundo', 'Efectivo', 1),
+(44752772, 'Mauricio Reta', '2003-04-13', '44752772', 'Efectivo', 1),
+(46807350, 'Jeremias Hoyo', '2005-11-21', 'jerechikito', 'Tarjeta Credito', 1);
 
 -- --------------------------------------------------------
 
@@ -95,7 +105,13 @@ CREATE TABLE `detalleticket` (
 
 INSERT INTO `detalleticket` (`codD`, `codProyeccion`, `total`, `codTicket`, `codAsiento`, `codAsiento2`, `estado`) VALUES
 (18, 18, 20000, 37, 68, 69, 1),
-(19, 19, 15000, 38, 70, NULL, 1);
+(19, 19, 15000, 38, 70, NULL, 1),
+(20, 18, 10000, 39, 71, NULL, 0),
+(21, 22, 10000, 40, 72, NULL, 0),
+(22, 19, 30000, 42, 73, 74, 0),
+(23, 19, 30000, 44, 75, 76, 0),
+(24, 19, 30000, 46, 77, 78, 0),
+(25, 19, 30000, 48, 79, 80, 0);
 
 -- --------------------------------------------------------
 
@@ -127,7 +143,8 @@ INSERT INTO `pelicula` (`titulo`, `director`, `actores`, `origen`, `genero`, `es
 ('Barbie', 'Greta Gerwig', 'Margot Robbie, Ryan Gosling, A', 'EE.UU.', 'Comedia', '2023-07-21', 1, 8),
 ('Roma', 'Alfonso Cuarón', 'Yalitza Aparicio, Marina de Ta', 'México', 'Drama', '2018-08-30', 1, 9),
 ('Spider-Man: No Way Home', 'Jon Watts', 'Tom Holland, Zendaya, Benedict', 'EE.UU.', 'Acción', '2021-12-17', 1, 10),
-('Relatos salvajes', 'Damián Szifron', 'Ricardo Darín, Érica Rivas, Le', 'Argentina', 'Drama', '2014-08-21', 1, 11);
+('Relatos salvajes', 'Damián Szifron', 'Ricardo Darín, Érica Rivas, Le', 'Argentina', 'Drama', '2014-08-21', 1, 11),
+('Laboratorio', 'Juan Saez', 'Juan Rodriguez, Agus Mazza', 'Argentina', 'Suspenso', '2025-11-18', 1, 12);
 
 -- --------------------------------------------------------
 
@@ -157,7 +174,10 @@ CREATE TABLE `proyeccion` (
 INSERT INTO `proyeccion` (`idioma`, `es3D`, `subtitulada`, `horaInicio`, `horaFin`, `lugaresDisponibles`, `precioDelLugar`, `nroSala`, `codProyeccion`, `idPelicula`, `estado`, `fechaFuncion`) VALUES
 ('Español', 0, 0, '16:00', '19:00', 15, 12000, 1, 17, 4, 1, '2025-11-19'),
 ('Ingles', 1, 1, '16:00', '18:00', 15, 10000, 2, 18, 8, 1, '2025-11-19'),
-('Español', 1, 0, '17:30', '19:00', 15, 15000, 3, 19, 10, 1, '2025-11-19');
+('Español', 1, 0, '17:30', '19:00', 15, 15000, 3, 19, 10, 1, '2025-11-19'),
+('Español', 0, 0, '16:30', '20:00', 15, 10000, 2, 20, 12, 127, '0000-00-00'),
+('Ingles', 1, 1, '16:30', '19:00', 15, 15000, 3, 21, 12, 127, '0000-00-00'),
+('Español', 0, 0, '16:00', '19:30', 15, 10000, 2, 22, 12, 1, '2025-11-19');
 
 -- --------------------------------------------------------
 
@@ -203,7 +223,17 @@ CREATE TABLE `ticketcompra` (
 INSERT INTO `ticketcompra` (`fechaCompra`, `codProyeccion`, `monto`, `dni`, `codTicket`, `estado`) VALUES
 ('2025-11-17', 18, 10000, 42357107, 36, 1),
 ('2025-11-17', 18, 10000, 42357107, 37, 1),
-('2025-11-21', 19, 15000, 42357107, 38, 1);
+('2025-11-21', 19, 15000, 42357107, 38, 1),
+('2025-11-18', 18, 10000, 38751080, 39, 1),
+('2025-11-18', 22, 10000, 42357107, 40, 1),
+('2025-11-18', 19, 15000, 42357107, 41, 1),
+('2025-11-18', 19, 15000, 42357107, 42, 1),
+('2025-11-18', 19, 15000, 42357107, 43, 1),
+('2025-11-18', 19, 15000, 42357107, 44, 1),
+('2025-11-18', 19, 15000, 42357107, 45, 1),
+('2025-11-18', 19, 15000, 42357107, 46, 1),
+('2025-11-18', 19, 15000, 42357107, 47, 1),
+('2025-11-18', 19, 15000, 42357107, 48, 1);
 
 --
 -- Índices para tablas volcadas
@@ -267,25 +297,25 @@ ALTER TABLE `ticketcompra`
 -- AUTO_INCREMENT de la tabla `asiento`
 --
 ALTER TABLE `asiento`
-  MODIFY `codAsiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+  MODIFY `codAsiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
 
 --
 -- AUTO_INCREMENT de la tabla `detalleticket`
 --
 ALTER TABLE `detalleticket`
-  MODIFY `codD` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `codD` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `pelicula`
 --
 ALTER TABLE `pelicula`
-  MODIFY `idPelicula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `idPelicula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `proyeccion`
 --
 ALTER TABLE `proyeccion`
-  MODIFY `codProyeccion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `codProyeccion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `sala`
@@ -297,7 +327,7 @@ ALTER TABLE `sala`
 -- AUTO_INCREMENT de la tabla `ticketcompra`
 --
 ALTER TABLE `ticketcompra`
-  MODIFY `codTicket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `codTicket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- Restricciones para tablas volcadas
